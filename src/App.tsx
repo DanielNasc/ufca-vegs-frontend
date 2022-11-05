6191019d08f1603aa146fe6fffa18fcd0b5a0853
DanielNasc <danielnasc15987@gmail.com>
2022-11-05 10:56:44 -0300

    initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <h1>
-      Hello,World!
-    </h1>
+    <ThemeProvider theme={defaultTheme} >
+      <h1>
+        Hello, World!
+      </h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
