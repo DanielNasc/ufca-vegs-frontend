7218c13fbe52e02972e657d8f992d901e2e973a4
DanielNasc <danielnasc15987@gmail.com>
2022-11-05 21:12:33 -0300

    Home Counter

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import { CountdownConatiner, HomeContainer } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <CountdownConatiner>
+        <span>0</span>
+        <span>0</span>
+        <span>0</span>
+        <span>0</span>
+      </CountdownConatiner>
+    </HomeContainer>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const CountdownConatiner = styled.div`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+
+  color: ${(props) => props.theme['gray-100']};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: ${(props) => props.theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
